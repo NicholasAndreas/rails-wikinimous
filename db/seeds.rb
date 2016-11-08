1be299cf9b77a83c855c310d8aa192c3de40c895
nicholassiskos <nicholasasiskos@gmail.com>
2016-11-08 16:02:49 +0000

    articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  post = Article.new({
+    title: Faker::Beer.name,
+    content: "#{Faker::Space.planet}, #{Faker::Address.city}",
+  })
+  post.save
+end
